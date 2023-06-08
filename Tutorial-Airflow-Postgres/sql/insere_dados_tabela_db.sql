05da8c66673b2f7d6e158d21dd5fa521d5a0494e
Rafael Porfirio <rafaporfirio.barros@gmail.com>
2023-06-08 11:14:00 -0300

    Add files via upload

---
+++ Tutorial-Airflow-Postgres/sql/insere_dados_tabela_db.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO TB_CLIENTE (name, dt)
+--                 values('Antonio Pires', CURRENT_DATE)
+            
+
+INSERT INTO TB_CLIENTE (name, dt)
+                values('Maria Bernardes', '{{ ds }}')
+            
